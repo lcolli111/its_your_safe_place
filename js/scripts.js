9c8c87fd6d62c812dcdad1f1ba3e9759d1127a97
lcolli111 <leenacolins4@yahoo.com>
2019-07-10 19:03:15 -0500

    event click

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-$(".bx").click(function() {
-    alert( "Handler for .click() called." );
+$(".bx").click(function(event) {
+    // $( "#foo" ).trigger( "click" );
+    console.log(event.delegateTarget);
+    // $( event.delegateTarget ).find("a").trigger("click");
+    // event.stopPropagation();
+    
+    // alert( "Handler for .click() called." );
+
+    // find the a tag inside the box
+    // get the "href" value from the a tag (attr)
+    // window location href 
+    // tell the brower to load another webpage
   });
